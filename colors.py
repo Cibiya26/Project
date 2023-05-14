4743e7addd269d2c27fb9a064ce992807181997a
Cibiya26 <132813468+Cibiya26@users.noreply.github.com>
2023-05-14 20:46:29 +0530

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,12 @@
+color_1 = "green"
+color_2 = "pink"
+color_3 = "black"
+color_4 = "white"
+
+font_1 = "arial"
+font_2 = "calibri"
+
+
+"""both fields empty - please enter this/that
+if someone fills both - one field is enough
+if any one of the field is filled - proceed"""
